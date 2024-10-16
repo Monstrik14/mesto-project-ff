9084a8cbccf691aee1b4a38e16ff0c3831054cb0
Sergei Suprun <sergeysuprun@list.ru>
2024-10-16 19:01:06 +0300

    16.10.2024

---
+++ src/index.js
@@ -0,0 +1,101 @@
+import './pages/index.css'
+
+import {initialCards} from './cards'
+
+// @todo: Темплейт карточки
+
+const cardTemplate = document.querySelector("#card-template").content;
+
+// @todo: DOM узлы
+
+const cardList = document.querySelector(".places__list");
+const buttonNewCard = document.querySelector(".profile__add-button");
+
+// @todo: Функция создания карточки
+
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", () => {
+      deleteCard(cardElement)
+    })
+  return cardElement;
+}
+
+// @todo: Функция удаления карточки
+
+  function deleteCard(cardElement) {
+    cardElement.remove();
+  }
+
+// @todo: Вывести карточки на страницу
+
+function cardRender(container, cardData) {
+  container.append(cardData);
+}
+
+initialCards.forEach(function(elem) {
+  cardRender(cardList, createCard(elem, deleteCard)); 
+}); 
+ 
+// edit profile
+
+const popupForm = document.querySelector('.popup_type_edit')
+const editProfileBtn = document.querySelector('.profile__edit-button')
+const closeBtn = document.querySelector('.popup__close')
+
+editProfileBtn.addEventListener('click', function (){
+  document.querySelector('.popup__input_type_name').value = 'Жак-Ив Кусто'
+  document.querySelector('.popup__input_type_description').value = 'Исследователь океана'
+  popupForm.classList.add('popup_is-opened') 
+})
+
+//close profile
+
+closeBtn.addEventListener('click', function (){
+  popupForm.classList.remove('popup_is-opened') 
+})
+
+//close by overlay
+
+window.onclick = function(evt){
+  if (evt.target === popupForm){
+    popupForm.classList.remove('popup_is-opened') 
+  }
+}
+
+//close by Esc
+
+document.addEventListener('keydown', function(evt) {
+  if (evt.key === "Escape") {
+    popupForm.classList.remove('popup_is-opened')
+  }
+});
+
+//form fields
+
+const formElement = document.querySelector('.popup_form')
+const nameInput = document.querySelector('.popup__input_type_name')
+const jobInput = document.querySelector('.popup__input_type_description')
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+
+    document.querySelector('.popup__input_type_name').value = nameForm
+    document.querySelector('.popup__input_type_description').value = descriptionForm
+
+  
+    
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
