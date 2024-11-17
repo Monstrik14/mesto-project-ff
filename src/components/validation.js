ef57acb48a1b43e867aa0aaac1d22dc3066b02a8
Sergei Suprun <sergeysuprun@list.ru>
2024-11-17 15:53:39 +0300

    -

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,31 +1,81 @@
 
-
-clearValidation(profileForm, validationConfig)
-
-// Вынесем все необходимые элементы формы в константы
-const formElement = document.querySelector('.popup__input_type_name')
-const formInput = formElement.querySelector('.popup__input_type_description')
-
-// Функция, которая добавляет класс с ошибкой
-const showInputError = (element) => {
-  element.classList.add('popup__input_type_error')
-};
-
-// Функция, которая удаляет класс с ошибкой
-const hideInputError = (element) => {
-  element.classList.remove('popup__input_type_error')
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
 }
 
-// Функция, которая проверяет валидность поля
-const isValid = () => {
-  if (!formInput.validity.valid) {
-    // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formInput)
-  } else {
-    // Если проходит, скроем
-    hideInputError(formInput)
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(validationConfig.formSelector))
+  formList.forEach((formElement) => {
+    setEventListeners(formElement, validationConfig)
+})
+}
+
+const setEventListeners = (formElement, validationConfig) => {
+  const inputList = Array.from(formElement.querySelectorAll(validationConfig.inputSelector))
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement, validationConfig)
+    })
+  })
+}
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+    inputElement.classList.add(validationConfig.inputErrorClass)
+    errorElement.textContent = errorMessage
+    errorElement.classList.add(validationConfig.errorClass)
+}
+
+const hideInputError = (formElement, inputElement, validationConfig) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+  inputElement.classList.remove(validationConfig.inputErrorClass)
+  errorElement.classList.remove(validationConfig.errorClass)
+  errorElement.textContent = ''
+}
+
+const isValid = (formElement, inputElement, validationConfig) => {
+  if (inputElement.validity.patternMismatch){
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage)
+    } else {
+      inputElement.setCustomValidity('')
+    }
   }
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage)
+  } else {
+    hideInputError(formElement, inputElement, validationConfig)
+  }
+
+const toggleButtonState = (inputList, buttonElement, validationConfig) => {
+  const hasInvalidInput = () => {
+  inputList.some((inputElement) => {
+    return !inputElement.validity.valid
+  })
+ if (hasInvalidInput(inputList)) {
+      buttonElement.classList.add(validation.inactiveButtonClass)
+      buttonElement.disabled = true
+  } else {
+      buttonElement.classList.remove(validation.inactiveButtonClass)
+      buttonElement.disabled = false
+  } 
+}
+}
+
+const clearValidation = () => {
+  const inputList = Array.from(formElement.querySelectorAll(validationConfig.inputSelector))
+  inputList.forEach((inputElement) => {
+    hideInputError(formElement, inputElement, validationConfig)
+    buttonElement.classList.add(validation.inactiveButtonClass)
+    buttonElement.disabled = true
+})
 }
 
 // Вызовем функцию isValid на каждый ввод символа***
-formInput.addEventListener('input', isValid) 
+inputElement.addEventListener('input', isValid) 
+
+clearValidation(profileForm, validationConfig)
--- src/index.js
+++ src/index.js
@@ -108,15 +108,6 @@ closeModal(newCardForm)
 
 newCardBtn.addEventListener('click', function(){
 openModal(newCardForm)
-}) 
-
-enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
 })
 
-/
+enableValidation(validation)
