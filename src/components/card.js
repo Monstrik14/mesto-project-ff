8f48fe9743657600b9faeafadb19e86d3384cdf4
Sergei Suprun <sergeysuprun@list.ru>
2024-11-26 22:28:19 +0300

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -42,7 +42,6 @@ function deleteCard(cardElement) {
 // card like
 
 function likeCard(evt) {
-  let count;
   if (evt.target.classList.contains('card__like-button')) 
   {evt.target.classList.toggle('card__like-button_is-active')}
 } 
